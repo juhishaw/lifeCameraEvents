4fbff3540ba2a2659f9c1f639ca9bae52035c6c0
juhishw <juhi.shaw@quovantis.com>
2020-07-20 11:50:24 +0530

    component structure

--- src/app/home/shared-home/shared-home.module.ts
+++ src/app/home/shared-home/shared-home.module.ts
@@ -3,9 +3,37 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SharedModule } from '../../shared/shared.module';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ServiceComponent } from './components/service/service.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ClientsComponent,
+    ServiceComponent,
+    TestimonialsComponent,
+    ContactUsComponent,
+    GalleryComponent,
+    SliderComponent,
+  ],
   imports: [CommonModule, SharedModule],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ClientsComponent,
+    ServiceComponent,
+    TestimonialsComponent,
+    ContactUsComponent,
+    GalleryComponent,
+    SliderComponent,
+  ],
 })
 export class SharedHomeModule {}
