8e0742d9df55fccd70dc431f0aa25c023ac33f72
juhishw <juhi.shaw@quovantis.com>
2020-07-20 11:23:07 +0530

    componet structure

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { SharedHomeModule } from './shared-home/shared-home.module';
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, SharedHomeModule, MaterialModule],
+})
+export class HomeModule {}
---
+++ src/app/home/shared-home/shared-home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SharedModule } from '../../shared/shared.module';
+
+@NgModule({
+  declarations: [NavbarComponent, FooterComponent],
+  imports: [CommonModule, SharedModule],
+})
+export class SharedHomeModule {}
